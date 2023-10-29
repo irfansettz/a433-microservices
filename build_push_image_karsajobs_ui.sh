9651429fd0bbc7e6f3bb59b2664dd270fdf9c30b
irfansettz <irfansettz@gmail.com>
2023-10-29 21:28:30 +0700

    +build images

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set GitHub username, repository name, and desired image tag
+USERNAME="irfansettz"
+REPOSITORY="karsajobs-ui"
+TAG="latest"
+
+# Use the GITHUB_TOKEN environment variable
+TOKEN="$GITHUB_TOKEN"
+
+# Check if the GITHUB_TOKEN is set
+if [ -z "$TOKEN" ]; then
+  echo "Error: GITHUB_TOKEN environment variable is not set."
+  exit 1
+fi
+
+# Authenticate Docker with GitHub Container Registry
+echo "$TOKEN" | docker login ghcr.io -u $USERNAME --password-stdin
+
+# Build the Docker image
+docker build -t ghcr.io/$USERNAME/$REPOSITORY:$TAG .
+
+# Push the Docker image to GitHub Container Registry
+docker push ghcr.io/$USERNAME/$REPOSITORY:$TAG
